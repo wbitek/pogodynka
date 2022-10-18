36af347ad19439b20d8a6b488abb1ec89cc6182d
Witek Bartłomiej <wb46689>
2022-10-18 14:44:45 +0200

    Add basic entities and their controllers

---
+++ src/Controller/WeatherController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Location;
+use App\Repository\MeasurementRepository;
+
+class WeatherController extends AbstractController
+{
+    // #[Route('/weather', name: 'app_weather')]
+    // public function index(): Response
+    // {
+    //     return $this->render('weather/index.html.twig', [
+    //         'controller_name' => 'WeatherController',
+    //     ]);
+    // }
+    public function cityAction(Location $city, MeasurementRepository $measurementRepository): Response
+    {
+        $measurements = $measurementRepository->findByLocation($city);
+        return $this->render('weather/city.html.twig', [
+            'location' => $city,
+            'measurements' => $measurements,
+        ]);
+    }
+}
---
+++ src/Entity/Location.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LocationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LocationRepository::class)]
+class Location
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $city = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $country = null;
+
+    #[ORM\Column(length: 3)]
+    private ?string $coutry_code = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getCountry(): ?string
+    {
+        return $this->country;
+    }
+
+    public function setCountry(string $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    public function getCoutryCode(): ?string
+    {
+        return $this->coutry_code;
+    }
+
+    public function setCoutryCode(string $coutry_code): self
+    {
+        $this->coutry_code = $coutry_code;
+
+        return $this;
+    }
+}
